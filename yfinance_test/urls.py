93d7b116788d4bef4b61bec93e3fe051a35fac6f
Pedro Maya Pires <pedromayapires@gmail.com>
2020-06-24 19:03:23 +0000

    Added 2 more endpoints Get recommendations still needs work, dont quite get what is being asked Added being possible to have to_date as optional, defaults it to the current day

--- yfinance_test/urls.py
+++ yfinance_test/urls.py
@@ -2,10 +2,20 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # server/yfinance_test/get_daily_price/msft/2020-01-01/2020-06-01
     url(
         r"^get_daily_price/(?P<ticker>\w+)/"
         + "(?P<from_date>[0-9]{4}-[0-9]{2}-[0-9]{2})"
         + "(/(?P<to_date>[0-9]{4}-[0-9]{2}-[0-9]{2}))?$",
         views.get_daily_price,
+    ),
+    # server/yfinance_test/get_company_info/msft
+    url(r"^get_company_info/(?P<ticker>\w+)$", views.get_company_info),
+    # server/yfinance_test/get_recommendations/msft/2020-01-01/2020-06-01
+    url(
+        r"^get_recommendations/(?P<ticker>\w+)/"
+        + "(?P<from_date>[0-9]{4}-[0-9]{2}-[0-9]{2})"
+        + "(/(?P<to_date>[0-9]{4}-[0-9]{2}-[0-9]{2}))?$",
+        views.get_recommendations,
     ),
 ]
--- yfinance_test/views.py
+++ yfinance_test/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import (
     # throttle_classes,
 )
 from django.http import JsonResponse
+from datetime import datetime
 import yfinance as yf
 
 from core.utils import exception_handler, LOGGER
@@ -12,7 +13,7 @@ from core.utils import exception_handler, LOGGER
 @api_view(["GET"])
 # @throttle_classes([UserRateThrottle])
 @exception_handler
-def get_daily_price(request, ticker, from_date, to_date):
+def get_daily_price(request, ticker, from_date, to_date=None):
     """ Return a timeseries with the price info of the company associated with
         a ticker for the specified start_date and end_date dates
     Args:
@@ -28,6 +29,11 @@ def get_daily_price(request, ticker, from_date, to_date):
         JsonResponse (object): Returns timeseries of the associated company
     """
     LOGGER.debug("Getting the daily price timeseries")
+
+    # if to_date is not set, this will default it to the current day
+    if not to_date:
+        to_date = datetime.today().strftime("%Y-%m-%d")
+
     # get the information
     data = yf.download(ticker, start=from_date, end=to_date)
 
@@ -45,4 +51,71 @@ def get_daily_price(request, ticker, from_date, to_date):
     # return_data = data.to_json()
 
     # i kept the adj close value, not sure if it's necessary
+    # can remove the extra data
+    return JsonResponse(return_data, safe=False)
+
+
+@api_view(["GET"])
+# @throttle_classes([UserRateThrottle])
+@exception_handler
+def get_company_info(request, ticker):
+    """ Get company info (sector, address, symbol/ticker, short name)
+    Args:
+        request (request): django request object, has user object among
+            other attributes
+        ticker (string): short name for a company
+
+    Returns:
+        JsonResponse (object): Returns the company information
+    """
+    LOGGER.debug("Get company info")
+    # get the information
+    ticker = yf.Ticker(ticker)
+    return_data = ticker.info
+
+    # i kept all data regarding the company, not sure if it's necessary
+    # can remove the extra data
+    return JsonResponse(return_data, safe=False)
+
+
+@api_view(["GET"])
+# @throttle_classes([UserRateThrottle])
+@exception_handler
+def get_recommendations(request, ticker, from_date, to_date=None):
+    """ Return a timeseries with average daily sentiment for the company
+        associated with a ticker for the specified start_date and end_date
+    Args:
+        request (request): django request object, has user object among
+            other attributes
+        ticker (string): short name for a company
+        from_date (string): starting date to get the information from in the
+            format yyyy-mm-dd
+        to_date (string): (optional) end date to get the information from in
+            the format yyyy-mm-dd, if not value is set, current date is used
+
+    Returns:
+        JsonResponse (object): Returns the company recommendations data
+    """
+    LOGGER.debug("Get the company recommendations data")
+
+    # if to_date is not set, this will default it to the current day
+    if not to_date:
+        to_date = datetime.today().strftime("%Y-%m-%d")
+
+    # get the recommendations data
+    ticker = yf.Ticker(ticker)
+
+    # this doesn't return what we want
+    # hist = ticker.history(start=from_date, end=to_date)
+
+    # From Grade and To Grade fields are the only ones that matter?
+    # is the idea to filter the dates obtained or is there a way to filter the
+    # data from the library itself?
+    data = ticker.recommendations
+
+    # convert the timestamp info
+    data.index = data.index.strftime("%Y-%m-%d")
+    # get the data in dictionary format
+    return_data = data.to_dict()
+
     return JsonResponse(return_data, safe=False)
