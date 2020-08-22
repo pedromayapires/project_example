7f26de225987b3fdd83481f786aa4198013f979c
Pedro Maya Pires <pedromayapires@gmail.com>
2020-08-22 16:39:51 +0000

    Added fixtures Minor changes to models

--- cv/models.py
+++ cv/models.py
@@ -10,24 +10,32 @@ class PersonalInfo(BaseModel):
     email = models.CharField(max_length=255)
     skype = models.CharField(max_length=255)
     linkedin = models.CharField(max_length=255)
+    languages = models.ManyToManyField(Skill)
 
 
 class Course(BaseModel):
     name = models.CharField(max_length=255, unique=True)
     # when did it start
-    from_date = models.DateTimeField()
+    from_date = models.DateField()
     # when did it end
-    to_date = models.DateTimeField()
+    to_date = models.DateField()
     # physical location
     location = models.CharField(max_length=255, unique=True)
 
 
 class LanguageLevel(BaseModel):
     name = models.CharField(max_length=255, unique=True)
+    # native is 1, fluent is 2 and so forth
+    rank = models.IntField()
 
 
 class Language(BaseModel):
     name = models.CharField(max_length=255, unique=True)
+
+
+class LanguageLearned(BaseModel):
+    person = models.ForeignKey(PersonalInfo, on_delete=models.PROTECT)
+    language = models.ForeignKey(Language, on_delete=models.PROTECT)
     # from bad to great or equivalent
     level = models.ForeignKey(LanguageLevel, on_delete=models.PROTECT)
 
@@ -43,9 +51,9 @@ class Skill(BaseModel):
 class Company(BaseModel):
     name = models.CharField(max_length=255, unique=True)
     # website
-    site = models.CharField(max_length=255, unique=True, null=True)
+    site = models.CharField(max_length=255, null=True)
     # physical location
-    location = models.CharField(max_length=255, unique=True, null=True)
+    location = models.CharField(max_length=255, null=True)
 
 
 class Project(BaseModel):
@@ -54,15 +62,16 @@ class Project(BaseModel):
     # what was accomplished or the purpose of the project
     accomplished = models.TextField()
     # when did it start
-    from_date = models.DateTimeField()
+    from_date = models.DateField()
     # when did it end
-    to_date = models.DateTimeField()
+    to_date = models.DateField()
 
+    # what was the consulting agency behind the client
+    agency = models.ForeignKey(Company, null=True, on_delete=models.PROTECT)
+    # what client, ideally there should be an option on the frontend to hide
+    # the client in cases where there is an agency involved
+    client = models.ForeignKey(Company, on_delete=models.PROTECT)
     # what roles were performed
     roles = models.ManyToManyField(Role)
     # what skills were used
     skills = models.ManyToManyField(Skill)
-    # what was the consulting agency behind the client
-    agency = models.ForeignKey(Company, null=True, on_delete=models.PROTECT)
-    # what client
-    client = models.ForeignKey(Company, on_delete=models.PROTECT)
